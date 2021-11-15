34aa7d929a674cba8d54496885e9c54ecff99f26
Sam Lorimer <slorimerjr@gmail.com>
2021-11-15 14:56:47 +0000

    It's not pretty but it works. First attempt.

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# must turn left or right -> right proceed; left die
+# must swim or wait -> swim drown; wait island
+# must choose a door -> R, B = death (different); Y = treasure
+# * "It's a room full of fire. Game Over."
+# * "You found the treasure! You Win!"
+# * "You enter a room of beasts. Game Over."
+# * "You chose a door that doesn't exist. Game Over."
+# * "You get attacked by an angry trout. Game Over."
+# * "You fell into a hole. Game Over."
+
 print('''
 *******************************************************************************
           |                   |                  |                     |
@@ -27,3 +37,23 @@ print("Your mission is to find the treasure.")
 
 #Write your code below this line 👇
 
+right_left = input('You\'re at a crossroad. Where do you want to go? Type "left" or "right" ').lower()
+if right_left == "right":
+  swim = input('''
+You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.
+''').lower()
+  if swim == "wait":
+    door = input('''You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? 
+''').lower()
+    if door == "yellow":
+      print("You found the treasure! You Win!")
+    elif door == "red":
+      print("It's a room full of fire. Game Over.")
+    else:
+      print("You enter a room of beasts. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fall into a hole and die.")
+
+# was having issues getting swim to print properly which is why i used the triple single quotes
